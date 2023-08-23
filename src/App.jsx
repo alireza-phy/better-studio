87aa45bf5f25b093bcda1904a7394b576b9a501a
Alireza Abbasi <alireza_physicist@yahoo.com>
2023-08-23 13:00:58 +0430

    setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () =>{
     return (
-        <h1>
+        <h1 class="text-6xl font-mono font-semibold text-red-600">
             Welcome to React App thats build using Webpack and Babel separately
         </h1>
     )
