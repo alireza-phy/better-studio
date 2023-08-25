967027b1c5408ec2aa90283eb52ab8769f743672
Alireza Abbasi <alireza_physicist@yahoo.com>
2023-08-25 21:08:57 +0430

    change mode in webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
    * the environment - development, production, none. tells webpack
    * to use its built-in optimizations accordingly. default is production
    */
-  mode: "development",
+  mode: "production",
   /** "entry"
    * the entry point
    */
